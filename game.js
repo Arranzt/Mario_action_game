91cbce3969656d23155733ce5c65c5e2e6a7847c
Arranzt <49115180+Arranzt@users.noreply.github.com>
2021-09-30 10:46:05 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 1,
+    scale: 1.5,
     debug: true,
     clearColor: [0, 0, 0, 1],
 });
@@ -48,7 +48,7 @@ scene("game", ({ level, score }) => {
         '                                     ',
         '     %   =*=%=                       ',
         '                           -+        ',
-        '                  ?   ?    ()        ',
+        '                  z   z    ()        ',
         '=============================    ====',
     ],
     [
